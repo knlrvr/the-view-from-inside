6230d665ce99d39541568ef78fea9182ce6d74ad
Kane <kanelariviere@outlook.com>
2023-05-02 10:59:06 -0400

    meta

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -16,7 +16,7 @@ export default function BlogHeader({
     case 1:
       return (
         <header className="mb-10 mt-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
-          <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
+          <h1 className="text-5xl font-bold leading-tight tracking-tighter md:pr-8 md:text-7xl">
             {title}
           </h1>
           <h4
